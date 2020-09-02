f0bbaa2b3b461b09edf8f39bddedd1081074da33
GameDog9988 <51346343+GameDog9988@users.noreply.github.com>
2020-09-01 20:44:06 -0400

    Import of GSAP

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "./App.css";
 import { Link } from "react-router-dom";
+import { TweenMax, Power3 } from "gsap";
 
 function Nav() {
+  let navBar = useRef(null);
+
+  console.log(navBar);
+
+  useEffect(() => {
+    console.log(navBar);
+    TweenMax.from();
+  }, []);
   return (
-    <nav>
+    <nav
+      ref={(el) => {
+        navBar = el;
+      }}
+    >
       <Link to="/" exact>
         <div className="logo">
           <span className="restaurant-name">Danny's</span>
