3e11d9065258785e1db74a16ff293ecee7b0ed8f
Alexander Grattan <alex@grattan.me>
2020-09-14 21:40:50 -0400

    Stagger Animation for Menu Items

--- src/Menu.js
+++ src/Menu.js
@@ -2,37 +2,6 @@ import React, { useRef, useEffect } from "react";
 import MenuItem from "./MenuItem";
 import "./App.css";
 import { TweenMax, Power3 } from "gsap";
-import Italian from "./images/eaters-collective-Gg5-K-mJwuQ-unsplash-min.jpg";
-import RoastBeef from "./images/pexels-erin-wang-1647163-min.jpg";
-import AvacadoToast from "./images/pexels-pixabay-236813-min.jpg";
-import Salad from "./images/youjeen-cho-sBKLiRiunK0-unsplash-min.jpg";
-
-const sandwiches = [
-  {
-    name: "Italian Sub",
-    price: "$7.99",
-    image: Italian,
-    index: 1,
-  },
-  {
-    name: "Chipotle Roast Beef",
-    price: "$9.99",
-    image: RoastBeef,
-    index: 2,
-  },
-  {
-    name: "Egg and Avacado Toast",
-    price: "$5.99",
-    image: AvacadoToast,
-    index: 3,
-  },
-  {
-    name: "Cranberry Pecan Salad",
-    price: "$7.99",
-    image: Salad,
-    index: 4,
-  },
-];
 
 function Menu() {
   let menuTitle = useRef(null);
@@ -49,16 +18,8 @@ function Menu() {
   return (
     <div className="menu-page">
       <h1 ref={(el) => (menuTitle = el)}>Menu</h1>
-      <div className="menu-container">
-        {sandwiches.map((sandwich) => (
-          <MenuItem
-            key={sandwich.index}
-            name={sandwich.name}
-            price={sandwich.price}
-            image={sandwich.image}
-            index={sandwich.index}
-          />
-        ))}
+      <div>
+        <MenuItem />
       </div>
     </div>
   );
--- src/MenuItem.js
+++ src/MenuItem.js
@@ -1,29 +1,67 @@
 import React, { useRef, useEffect } from "react";
 import "./App.css";
 import { TweenMax, Power3 } from "gsap";
+import Italian from "./images/eaters-collective-Gg5-K-mJwuQ-unsplash-min.jpg";
+import RoastBeef from "./images/pexels-erin-wang-1647163-min.jpg";
+import AvacadoToast from "./images/pexels-pixabay-236813-min.jpg";
+import Salad from "./images/youjeen-cho-sBKLiRiunK0-unsplash-min.jpg";
 
-function MenuItem({ name, price, image, index }) {
-  let menuItem = useRef();
-  console.log(menuItem);
+const sandwiches = [
+  {
+    name: "Italian Sub",
+    price: "$7.99",
+    image: Italian,
+    index: 1,
+  },
+  {
+    name: "Chipotle Roast Beef",
+    price: "$9.99",
+    image: RoastBeef,
+    index: 2,
+  },
+  {
+    name: "Egg and Avacado Toast",
+    price: "$5.99",
+    image: AvacadoToast,
+    index: 3,
+  },
+  {
+    name: "Cranberry Pecan Salad",
+    price: "$7.99",
+    image: Salad,
+    index: 4,
+  },
+];
+
+function MenuItem() {
+  let item = useRef([]);
+  console.log(item);
 
   useEffect(() => {
-    TweenMax.from(menuItem, 0.8, {
+    TweenMax.from(item.current, 0.8, {
       opacity: 0,
-      x: -100,
+      y: 50,
       ease: Power3.easeOut,
-      delay: 1.2,
+      delay: 1,
+      stagger: 0.2,
     });
-  });
+  }, []);
 
   return (
-    <div
-      className="menu-item-container"
-      key={index}
-      ref={(el) => (menuItem = el)}
-    >
-      <img src={image} alt={name} />
-      <h3>{name}</h3>
-      <p>{price}</p>
+    <div className="menu-container">
+      {sandwiches.map((sandwich, i) => (
+        <div
+          className="menu-item-container"
+          key={sandwich.index}
+          ref={(e) => {
+            item.current[i] = e;
+          }}
+        >
+          <img src={sandwich.image} alt={sandwich.name} />
+          <h3>{sandwich.name}</h3>
+          <p>{sandwich.price}</p>
+        </div>
+      ))}
     </div>
   );
 }
